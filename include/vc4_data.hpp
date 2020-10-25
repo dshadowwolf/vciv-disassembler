f21725fb6c925f245c9df9005a4a5fc807ed1f06
Daniel Hazelton <dshadowwolf@gmail.com>
2020-10-25 02:29:03 -0400

    minor data changes and reformatting

--- include/vc4_data.hpp
+++ include/vc4_data.hpp
@@ -7,77 +7,59 @@
 using namespace std;
 
 namespace disasm {
-    const string al_ops[] = { "mov", "cmn", "add", "bic", "mul", "eor", "sub",
-                              "and", "not", "ror", "cmp", "rsub", "btest", "or",
-                              "bmask", "max", "bitset", "min", "bitclear",
-                              "addscale", "bitflip", "addscale", "addscale",
-                              "addscale", "signext", "neg", "lsr", "msb", "shl",
-                              "brev", "asr", "abs", "mulhd.ss", "mulhd.su",
-                              "mulhd.us", "mulhd.uu", "div.ss", "div.su",
-                              "div.us", "div.uu", "adds", "subs", "shls",
-                              "clipsh", "addscale", "addscale", "addscale",
-                              "addscale", "count", "subscale", "subscale",
-                              "subscale", "subscale", "subscale", "subscale",
-                              "subscale", "subscale" };
-    
-    const string condition_codes[] = { "eq", "ne", "cs", "cc", "ns", "nc", "vs",
-                                 "vc", "gt", "lte", "gte", "lt", "gt",
-                                       "lte", "ra", ".never" };
-    
-    const string mem_op_widths[] = { "", "h", "b", "s"};
+	const string al_ops[] = { "mov", "cmn", "add", "bic", "mul", "eor", "sub",
+		"and", "not", "ror", "cmp", "rsub", "btest", "or",
+		"bmask", "max", "bitset", "min", "bitclear",
+		"addscale", "bitflip", "addscale", "addscale",
+		"addscale", "signext", "neg", "lsr", "msb", "shl",
+		"brev", "asr", "abs", "mulhd.ss", "mulhd.su",
+		"mulhd.us", "mulhd.uu", "div.ss", "div.su",
+		"div.us", "div.uu", "adds", "subs", "shls",
+		"clipsh", "addscale", "addscale", "addscale",
+		"addscale", "count", "subscale", "subscale",
+		"subscale", "subscale", "subscale", "subscale",
+		"subscale", "subscale" };
 
-    const string float_ops[] = { "fadd", "fsub", "fmul", "fabs", "frsub",
-                                 "fmax", "frcp", "frsqrt", "fnmul", "fmin",
-                                 "fceil", "ffloor", "flog2", "fexp2" };
-    
-    const string vector_widths[] = { "8", "16", "32", "[8]" };
-    
-    const string vector_ops_48[] = { "ld", "lookupmh", "lookupml",
-                                     "undefined(00011)", "st", "indexwritemh",
-                                     "indexwriteml",    "undefined(00111)",
-                                     "readlut", "writelut",
-                                     "undefined(01010)",
-                                     "undefined(01011)", "undefined(01100)",
-                                     "undefined(01101)", "undefined(01110)",
-                                     "undefined(01111)", "undefined(10000)",
-                                     "undefined(10001)", "undefined(10010)",
-                                     "undefined(10011)", "undefined(10100)",
-                                     "undefined(10101)", "undefined(10110)",
-                                     "undefined(10111)", "readacc",
-                                     "undefined(11001)", "undefined(11011)",
-                                     "undefined(11100)", "undefined(11101)",
-                                     "undefined(11110)", "undefined(11111)" };
-    
-    const string vector_flags_w[] = { "", "NV", "IFNZ", "IFN", "IFNN", "IFC", "IFNC" };
-    
-    const string vector_ops_full[] = { "vmov", "vbitplanes", "veven", "vodd",
-                                       "vinterl", "vinterh", "vbitrev", "vror",
-                                       "vshl", "vshls", "vlsr", "vasr",
-                                       "vsignhl", "vop13[unused]", "vsignasl",
-                                       "vsignasls", "vand", "vor", "vxor",
-                                       "vbic", "vcount", "vmsb",
-                                       "vop22[unused]", "vop23[unused]", "vmin",
-                                       "vmax", "vdist", "vdists", "vclip",
-                                       "vsign", "vclips", "vtestmag", "vadd",
-                                       "vadds", "vaddc", "vaddsc", "vsub",
-                                       "vsubs", "vsubc", "vsubsc", "vrsub",
-                                       "vrsubs", "vrsubc", "vrsubsc",
-                                       "vop44[unused]", "vop45[unused]",
-                                       "vop46[unused]", "vop47[unused]",
-                                       "vmull.ss", "vmulls.ss", "vmulm.ss",
-                                       "vmulms.ss", "vmulhd.ss", "vmulhd.su",
-                                       "vmulhd.us", "vmulhd.uu", "vmulhn.ss",
-                                       "vmulhn.su", "vmulhn.us", "vmulhn.uu",
-                                       "vmuldt.ss", "vmuldt.su",
-                                       "vop62[unused]", "vop63[unused]" };
-    const string vector48_alts[] = { "vop48[unused]", "vop49[unused]",
-                                     "vop50[unused]", "vop51[unused]",
-                                     "vmul32.ss", "vmul32.su", "vmul32.us",
-                                     "vmul32.uu",  "vop56[unused]",
-                                     "vop57[unused]", "vop58[unused]",
-                                     "vop59[unused]", "vop60[unused]",
-                                     "vop61[unused]", "vop62[unused]",
-                                     "vop63[unused]" };
+	const string condition_codes[] = { "eq", "ne", "cs", "cc", "ns", "nc", "vs",
+		"vc", "gt", "lte", "gte", "lt", "gt",
+		"lte", "ra", ".never" };
+
+	const string mem_op_widths[] = { "", "h", "b", "s"};
+
+	const string float_ops[] = { "fadd", "fsub", "fmul", "fabs", "frsub",
+		"fmax", "frcp", "frsqrt", "fnmul", "fmin",
+		"fceil", "ffloor", "flog2", "fexp2" };
+
+	const string vector_widths[] = { "8", "16", "32", "[8]" };
+
+	const string vector_ops_48[] = { "ld", "lookupmh", "lookupml",
+		"mem03[unused]", "st", "indexwritemh", "indexwriteml", "mem07[unused]",
+		"memread", "memwrite", "mem10[unused]",	"mem11[unused]", "mem12[unused]",
+		"mem13[unused]", "mem14[unused]", "mem15[unused]", "mem16[unused]",
+		"mem17[unused]", "mem18[unused]", "mem19[unused]", "mem20[unused]",
+		"mem21[unused]", "mem22[unused]", "mem23[unused]", "getacc",
+		"mem25[unused]", "mem26[unused]", "mem27[unused]", "mem28[unused]",
+		"mem29[unused]", "mem30[unused]", "mem31[unused]" };
+
+	const string vector_flags_w[] = { "", "NV", "IFNZ", "IFN", "IFNN", "IFC", "IFNC" };
+
+	const string vector_ops_full[] = { "mov", "bitplanes", "even", "odd", "interl",
+		"interh", "brev", "ror", "shl", "shls", "lsr", "asr", "signshl", "op13[unused]",
+		"signasl", "signasls", "and", "or", "eor", "bic", "count", "msb","op22[unused]",
+		"op23[unused]", "min", "max", "dist", "dists", "clip", "sign", "clips",
+		"testmag", "add", "adds", "addc", "addsc", "sub", "subs", "subc", "subsc",
+		"rsub", "rsubs", "rsubc", "rsubsc", "op44[unused]", "op45[unused]",
+		"op46[unused]", "op47[unused]", "mull", "mulls", "mulm", "mulms", "mulhd.ss",
+		"mulhd.su", "mulhd.us", "mulhd.uu", "mulhn.ss", "mulhn.su", "mulhn.us",
+		"mulhn.uu", "mulht.ss", "mulht.su", "op62[unused]", "op63[unused]" };
+
+	const string vector48_alts[] = { "op48[unused]", "op49[unused]", "op50[unused]",
+		"op51[unused]",	"mul32.ss", "mul32.su", "mul32.us",	"mul32.uu", "op56[unused]",
+		"op57[unused]", "op58[unused]",	"op59[unused]", "op60[unused]",	"op61[unused]",
+		"op62[unused]",	"op63[unused]" };
+
+	const string vector_reps[] = { "",  "REP2", "REP4", "REP8", "REP16", "REP32", "REP64", "REP r0" };
+
 }
 
 #endif // __VC4_DATA_H_
